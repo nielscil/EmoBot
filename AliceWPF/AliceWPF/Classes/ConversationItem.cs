43ad601ac932625ada16be97871140836dd3c778
DubstepJoris <dubstepjoris@hotmail.com>
2017-05-10 12:17:00 +0200

    Klasse ConversationItem toegevoegd

---
+++ AliceWPF/AliceWPF/Classes/ConversationItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceWPF.Classes
+{
+    class ConversationItem
+    {
+        // Public variables
+        public enum Sender { Bot, User };
+
+        // Private variables
+        private Sender sender;
+        private string content;
+
+        public void setSender(Sender sender)
+        {
+
+        }
+
+        public Sender getSender()
+        {
+            return Sender.Bot;
+        }
+    }
+}
