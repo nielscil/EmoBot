15342932ffaaa0204cf00ae7918cc78fae93d330
DubstepJoris <dubstepjoris@hotmail.com>
2017-05-10 11:30:53 +0200

    Start Alice WPF (Material design)

---
+++ AliceWPF/AliceWPF/Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using AliceWPF.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AliceWPF.Bootstrapper
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ AliceWPF/AliceWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceWPF.ViewModels
+{
+    class MainViewModel : PropertyChangedBase
+    {
+        private string _input;
+        public string Input
+        {
+            get
+            {
+                return _input;
+            }
+            set
+            {
+                _input = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
