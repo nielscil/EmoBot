9a6d92a2ba6fd528d46de04456ca4e947f215510
nielscil <niels.bouma@outlook.com>
2017-05-10 12:07:36 +0200

    added begin to converstation Control

---
+++ AliceWPF/AliceWPF/Controls/ConverstationItemControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AliceWPF.Controls
+{
+    public partial class ConverstationItemControl : UserControl, IDisposable
+    {
+
+        public ConverstationItemControl()
+        {
+            InitializeComponent();
+            DataContextChanged += ConverstationItemControl_DataContextChanged;
+        }
+
+        public void Dispose()
+        {
+            DataContextChanged -= ConverstationItemControl_DataContextChanged;
+        }
+
+        private void ConverstationItemControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Card card = new Card();
+            //TODO: set good grid column
+            //TOOD: add textblock
+
+            Grid grid = Content as Grid;
+            if(grid != null)
+            {
+                grid.Children.Add(card);
+            }
+        }
+    }
+}
