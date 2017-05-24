951889cbb3523012c7f2bfae577cec82c7af1058
nielscil <niels.bouma@outlook.com>
2017-05-24 14:04:07 +0200

    added data files for affectivia

---
+++ AliceWPF/EmotionLib/EmotionDetector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionLib
+{
+    public class EmotionDetector
+    {
+        #region singleton
+
+        private static EmotionDetector _instance;
+        public static EmotionDetector Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new EmotionDetector();
+                }
+                return _instance;
+            }
+        }
+
+        private EmotionDetector() { }
+
+        #endregion
+
+        private bool _isInitialized = false;
+
+        public void Init()
+        {
+            if(_isInitialized)
+            {
+                throw new Exception("EmotionDetector is already Initialized");
+            }
+
+        }
+
+        public void Start()
+        {
+            CheckInitalized();
+        }
+
+        public void Stop()
+        {
+            CheckInitalized();
+        }
+
+        private void CheckInitalized()
+        {
+            if(!_isInitialized)
+            {
+                throw new Exception("EmotionDetector is not Initialized, call Init() first");
+            }
+        }
+
+    }
+}
