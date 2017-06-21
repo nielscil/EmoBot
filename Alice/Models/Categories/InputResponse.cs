c46cb79133723d918a765bed93f52e18654f5b00
LosKjeldos <wazzahh-_-@hotmail.com>
2017-06-21 10:48:10 +0200

    Matcher merged with Inputresponse
    Didnt serve an extra purpose other than being part of inpuresponse.

--- Alice/Models/Categories/InputResponse.cs
+++ Alice/Models/Categories/InputResponse.cs
@@ -7,9 +7,39 @@ using System.Threading.Tasks;
 
 namespace Alice.Models.Categories
 {
-    public class InputResponse : Matcher
+    public class InputResponse
     {
+        private List<string> patternsToMatch = new List<string>();
+        List<Tuple<int, string>> previousResponsePatternsToMatch = new List<Tuple<int, string>>();
+
         private TemplateBase _template;
+
+        public void addPattern(string pattern)
+        {
+            patternsToMatch.Add(pattern);
+        }
+
+        public void AddPreviousResponsePattern(int dept, string pattern)
+        {
+            previousResponsePatternsToMatch.Add(new Tuple<int, string>(dept, pattern));
+        }
+
+        public bool isMatch(InputResponseData inputResponseData)
+        {
+            foreach (var pattern in patternsToMatch)
+            {
+                Match match = Regex.Match(inputResponseData.input, pattern);
+                List<InputResponseData> data;
+
+                if (match.Success && InputResponseManager.IsMatchingPreviousResponses(previousResponsePatternsToMatch, out data))
+                {
+                    inputResponseData.Match = match;
+                    inputResponseData.PreviousResponseData = data;
+                    return true;
+                }
+            }
+            return false;
+        }
 
         public void SetTemplate(TemplateBase template)
         {
